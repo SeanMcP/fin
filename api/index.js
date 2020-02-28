66481a148288ae4f6385bbb7cf38d7d233dce91b
Sean McPherson <git@seanmcp.com>
2020-02-28 14:28:27 -0500

    Add GET /user, POST /users endpoints

--- api/index.js
+++ api/index.js
@@ -1,7 +1,38 @@
 const express = require('express')
 const app = express()
+const { Pool } = require('pg')
 const port = 3001
 
+const pool = new Pool({
+    host: 'localhost',
+    database: 'fin',
+    port: 5432
+})
+
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/users', (req, res) => {
+    pool.query('SELECT * from users')
+        .then(response => {
+            console.log(response)
+            res.send('Success')
+        })
+        .catch(error => {
+            console.log(error)
+            res.send('Error')
+        })
+})
+
+app.post('/user', (req, res) => {
+    pool.query('INSERT INTO users (name) VALUES (\'Test\')')
+        .then(response => {
+            console.log(response)
+            res.send('Success')
+        })
+        .catch(err => {
+            console.log(err)
+            res.send('Error')
+        })
+})
 
 app.listen(port, () => console.log(`API listening on port ${port}!`))
