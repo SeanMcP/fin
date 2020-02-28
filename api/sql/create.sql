51ee69dd24242ba60013e41d40081222cc4ebee2
Sean McPherson <git@seanmcp.com>
2020-02-28 13:34:50 -0500

    Update create script
    Creates the tables

--- api/sql/create.sql
+++ api/sql/create.sql
@@ -1,6 +1,21 @@
-CREATE DATABASE dev;
-
 CREATE TABLE users (
-    id int,
-    name varchar
-)
+    id SERIAL PRIMARY KEY
+    -- name TEXT
+);
+
+CREATE TABLE classes (
+    id SERIAL PRIMARY KEY,
+    -- name TEXT
+    user_id INTEGER REFERENCES users(id)
+);
+
+CREATE TABLE students (
+    id SERIAL PRIMARY KEY,
+    -- name TEXT,
+    user_id INTEGER REFERENCES users(id)
+);
+
+CREATE TABLE spots (
+    class_id INTEGER REFERENCES classes(id),
+    student_id INTEGER REFERENCES students(id)
+);
