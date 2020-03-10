f7179214c8990df5f092191577763ed12f10a023
Sean McPherson <git@seanmcp.com>
2020-03-09 22:12:41 -0400

    Add pbkdf2 constants

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,10 +1,13 @@
 const crypto = require('crypto')
 const db = require('../db')
 
+const iterations = 100000
+const keyLength = 64
+const digest = 'sha512'
+
 function register(req, res) {
     const salt = crypto.randomBytes(128).toString('base64')
-    /** pbkdf2Sync params: password, salt, iterations, key length, digest */
-    const hash = crypto.pbkdf2Sync(req.body.password, salt, 100000, 64, 'sha512').toString('hex')
+    const hash = crypto.pbkdf2Sync(req.body.password, salt, iterations, keyLength, digest).toString('hex')
 
     db.query(`INSERT INTO users (email, password, nonce) VALUES ('${req.body.email}', '${hash}', '${salt}')`)
         .then(response => {
