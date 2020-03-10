335acecc95376c8d70f6b60d075defb52e80cd9c
Sean McPherson <git@seanmcp.com>
2020-03-09 22:38:46 -0400

    Add getHash function
    Clean as you like.

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -4,13 +4,18 @@ const db = require('../db')
 const iterations = 100000
 const keyLength = 64
 const digest = 'sha512'
+const stringType = 'hex'
+
+function getHash(password, nonce) {
+    return crypto.pbkdf2Sync(password, nonce, iterations, keyLength, digest).toString(stringType)
+}
 
 function login(req, res) {
     db.query(`SELECT email, nonce, password FROM users WHERE email = '${req.body.email}'`)
         .then(response => {
             const [user] = response.rows
             if (user) {
-                if (crypto.pbkdf2Sync(req.body.password, user.nonce, iterations, keyLength, digest).toString('hex') === user.password) {
+                if (getHash(req.body.password, user.nonce) === user.password) {
                     res.send({ success: true })
                 }
             }
@@ -23,7 +28,7 @@ function login(req, res) {
 
 function register(req, res) {
     const salt = crypto.randomBytes(128).toString('base64')
-    const hash = crypto.pbkdf2Sync(req.body.password, salt, iterations, keyLength, digest).toString('hex')
+    const hash = getHash(req.body.password, salt)
 
     db.query(`INSERT INTO users (email, password, nonce) VALUES ('${req.body.email}', '${hash}', '${salt}')`)
         .then(response => {
