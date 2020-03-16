0c3538ae3a79c3c8fbef3c9698c63875cbdfc8f0
Sean McPherson <git@seanmcp.com>
2020-03-16 06:46:46 -0400

    Update responses

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,38 +1,32 @@
 const db = require('../db')
 
-function add (req, res) {
+function add(req, res) {
     db.query(`INSERT INTO users (name) VALUES (${req.body.name})`)
         .then(response => {
-            console.log(response)
-            res.send('Success')
+            res.send({ data: response.rows })
         })
-        .catch(err => {
-            console.log(err)
-            res.send('Error')
+        .catch(error => {
+            res.send({ error })
         })
 }
 
-function getAll (req, res) {
+function getAll(req, res) {
     db.query('SELECT * FROM users')
         .then(response => {
-            console.log(response)
-            res.send('Success')
+            res.send({ data: response.rows })
         })
         .catch(error => {
-            console.log(error)
-            res.send('Error')
+            res.send({ error })
         })
 }
 
 function updateById(req, res) {
     db.query(`UPDATE users SET name = '${req.body.name}' WHERE id = ${req.params.id}`)
         .then(response => {
-            console.log(response)
-            res.send('Success')
+            res.send({ data: response })
         })
         .catch(error => {
-            console.log(error)
-            res.send('Error')
+            res.send({ error })
         })
 }
 
