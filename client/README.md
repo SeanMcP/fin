f638527a92b96863f0e6bfa5410f4fae174557d9
Sean McPherson <git@seanmcp.com>
2020-03-23 06:26:37 -0400

    Add client with create-react-app

---
+++ client/README.md
@@ -0,0 +1,3 @@
+# Client
+
+The React client for Fin
---
+++ client/src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App__header">
+        Fin
+      </header>
+      <main>
+          Hello world!
+      </main>
+    </div>
+  );
+}
+
+export default App;
