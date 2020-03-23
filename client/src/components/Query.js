1eb04e9b62dcbacf64f6fcf5beb6e3a498dfba6a
Sean McPherson <git@seanmcp.com>
2020-03-23 07:01:49 -0400

    Add Query component
    This may be a bad idea, but it feels like a good way to manage request states.

---
+++ client/src/components/Query.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useQuery } from 'react-query'
+
+function Query(props) {
+    const { status, data, error } = useQuery(props.id, () =>
+        fetch(props.route)
+    )
+
+    if (status === 'loading') {
+        return typeof props.onLoading === 'function' ? props.onLoading() : <span>Loading...</span>
+    }
+
+    if (status === 'error') {
+        return typeof props.onError === 'function' ? props.onError(error) : <span>Uh oh! There was a problem loading.</span>
+    }
+
+    return typeof props.children === 'function' ? props.children(data) : <span>Data: <pre>{JSON.stringify(data, null, 2)}</pre></span>
+}
+
+export default Query
