97d655b091cd2c2de671407a8f9531ba3b5e9162
Sean McPherson <git@seanmcp.com>
2020-03-24 06:57:49 -0400

    Hit /register with form
    This doesn't work well in the app, but it functions to register a user.

--- client/src/screens/Home.js
+++ client/src/screens/Home.js
@@ -7,6 +7,16 @@ function Home() {
         <ScreenLayout>
             Hello from <code>/home</code>
             <Query id="users" route="http://localhost:3001/users" />
+            <form action="http://localhost:3001/register" method="post">
+                <h2>Register</h2>
+                <div>
+                    <label>Email<br /><input type="text" name="email" /></label>
+                </div>
+                <div>
+                    <label>Password<br /><input type="password" name="password" /></label>
+                </div>
+                <button>Register</button>
+            </form>
         </ScreenLayout>
     )
 }
