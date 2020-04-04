044228fef925a85905fe79927d2017e25d7455af
Sean McPherson <git@seanmcp.com>
2020-04-03 23:29:54 -0400

    Standardize student handlers responses

--- api/routes/students.js
+++ api/routes/students.js
@@ -1,37 +1,38 @@
 const db = require('../db')
+const logger = require('../logging/logger')
 
 function add(req, res) {
   db.query('INSERT INTO students (name, user_id) VALUES ($1, $2)', [
     req.body.name,
     req.body.userId,
   ])
-    .then((response) => {
-      console.log(response)
+    .then(() => {
       res.send({ success: true })
     })
     .catch((error) => {
-      console.log(error)
-      res.send('Error')
+      logger.error('students > add()', error)
+      res.send({ error })
     })
 }
 
 function getAll(req, res) {
   db.query('SELECT * FROM students')
     .then((response) => {
-      console.log(response)
-      res.status(200).send({ data: response.rows })
+      res.send({ students: response.rows })
     })
     .catch((error) => {
-      res.status(404).send({ error, title: 'Unable to retrieve data' })
+      logger.error('students > getAll()', error)
+      res.send({ error })
     })
 }
 
 function getAllByUserId(req, res) {
   db.query('SELECT * FROM students WHERE user_id = $1', [req.params.userId])
     .then((response) => {
-      res.send({ data: response.rows })
+      res.send({ students: response.rows })
     })
     .catch((error) => {
+      logger.error('students > getAllByUserId()', error)
       res.send({ error })
     })
 }
@@ -39,11 +40,11 @@ function getAllByUserId(req, res) {
 function getById(req, res) {
   db.query(`SELECT * FROM students WHERE id = $1`, [req.params.id])
     .then((response) => {
-      res.send({ data: response.rows })
+      res.send({ student: response.rows })
     })
     .catch((error) => {
-      console.log(error)
-      res.send('Error')
+      logger.error('students > getById()', error)
+      res.send({ error })
     })
 }
 
@@ -52,25 +53,23 @@ function updateById(req, res) {
     req.body.name,
     req.params.id,
   ])
-    .then((response) => {
-      console.log(response)
+    .then(() => {
       res.send({ success: true })
     })
     .catch((error) => {
-      console.log(error)
-      res.send('Error')
+      logger.error('students > updateById()', error)
+      res.send({ error })
     })
 }
 
 function deleteById(req, res) {
   db.query('DELETE students WHERE id = $1', [req.params.id])
-    .then((response) => {
-      console.log(response)
+    .then(() => {
       res.send({ success: true })
     })
     .catch((error) => {
-      console.log(error)
-      res.send('Error')
+      logger.error('students > deleteById()', error)
+      res.send({ error })
     })
 }
 
