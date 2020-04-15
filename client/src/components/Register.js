3234b2f5f3cb7b031ee3f22e6c35c394b00d5897
Sean McPherson <git@seanmcp.com>
2020-04-15 07:00:46 -0400

    Convert register form to formik

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,30 +1,32 @@
 import React from 'react'
+import { useFormik } from 'formik'
 import request from '../utils/request'
 
 function Register() {
-    const [email, setEmail] = React.useState('chuck@testa.com')
-    const [password, setPassword] = React.useState('nope')
+    const formik = useFormik({
+        initialValues: {
+            email: `chuck+${String(Math.random()).slice(-5)}@testa.com`,
+            password: 'nope'
+        },
+        async onSubmit({ email, password }) {
+            const response = await request('register', {
+                body: { email, password }
+            })
 
-    async function handleSubmit(event) {
-        event.preventDefault()
-
-        const response = await request('register', {
-            body: { email, password }
-        })
-
-        if (response.ok) {
-            alert('You have successfully regsitered an account. Welcome aboard!')
+            if (response.ok) {
+                alert('You have successfully regsitered an account. Welcome aboard!')
+            }
         }
-    }
+    })
 
     return (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={formik.handleSubmit}>
             <h2>Register</h2>
             <div>
-                <label>Email<br /><input type="text" name="email" value={email} onChange={e => setEmail(e.target.value)} /></label>
+                <label>Email<br /><input type="text" name="email" value={formik.values.email} onChange={formik.handleChange} /></label>
             </div>
             <div>
-                <label>Password<br /><input type="password" name="password" value={password} onChange={e => setPassword(e.target.value)} /></label>
+                <label>Password<br /><input type="password" name="password" value={formik.values.password} onChange={formik.handleChange} /></label>
             </div>
             <button>Register</button>
         </form>
