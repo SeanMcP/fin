091adbb37332104f5df89727ca9a9471c90d859c
Sean McPherson <git@seanmcp.com>
2020-04-15 06:57:29 -0400

    Convert log in form to formik

--- client/src/components/LogIn.js
+++ client/src/components/LogIn.js
@@ -1,36 +1,39 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
+import { useFormik } from 'formik'
 import { useAppContext, APP_ACTIONS } from '../store/AppContext'
 import request from '../utils/request'
 
 function LogIn() {
-    const [email, setEmail] = React.useState('chuck@testa.com')
-    const [password, setPassword] = React.useState('nope')
     const [, dispatch] = useAppContext()
     const history = useHistory()
 
-    async function handleSubmit(event) {
-        event.preventDefault()
+    const formik = useFormik({
+        initialValues: {
+            email: 'chuck@testa.com',
+            password: 'nope'
+        },
+        async onSubmit({ email, password }) {
+            const response = await request('login', {
+                body: { email, password }
+            })
 
-        const response = await request('login', {
-            body: { email, password }
-        })
-
-        if (response.ok) {
-            const res = await response.json()
-            dispatch({ type: APP_ACTIONS.LOG_IN, payload: { user: res.user } })
-            history.push('/dashboard')
+            if (response.ok) {
+                const { user } = await response.json()
+                dispatch({ type: APP_ACTIONS.LOG_IN, payload: { user } })
+                history.push('/dashboard')
+            }
         }
-    }
+    })
 
     return (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={formik.handleSubmit}>
             <h2>Log In</h2>
             <div>
-                <label>Email<br /><input type="text" name="email" value={email} onChange={e => setEmail(e.target.value)} /></label>
+                <label>Email<br /><input type="text" name="email" value={formik.values.email} onChange={formik.handleChange} /></label>
             </div>
             <div>
-                <label>Password<br /><input type="password" name="password" value={password} onChange={e => setPassword(e.target.value)} /></label>
+                <label>Password<br /><input type="password" name="password" value={formik.values.password} onChange={formik.handleChange} /></label>
             </div>
             <button>Log In</button>
         </form>
