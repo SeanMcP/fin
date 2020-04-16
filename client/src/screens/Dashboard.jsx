d586c4e925aa04ab37c3e633a5a1f3e46c114cd3
Sean McPherson <git@seanmcp.com>
2020-04-16 07:00:15 -0400

    Add class nav to dashboard

--- client/src/screens/Dashboard.jsx
+++ client/src/screens/Dashboard.jsx
@@ -3,6 +3,7 @@ import ScreenLayout from '../components/ScreenLayout'
 import Query from '../components/Query'
 import { useAppContext } from '../store/AppContext'
 import AddClass from '../components/AddClass'
+import { Link } from 'react-router-dom'
 
 function Dashboard() {
     const [{ user }] = useAppContext()
@@ -10,7 +11,15 @@ function Dashboard() {
         <ScreenLayout title="Dashboard">
             <h1>Dashboard</h1>
             <pre>{JSON.stringify(user, null, 2)}</pre>
-            <Query id="classes" route={`/classes/${user.id}`} />
+            <Query id="classes" route={`/classes/${user.id}`}>
+                {({ data }) => (
+                    <nav aria-label="classes navigation">
+                        <ul>
+                            {data.map(({ id, name }) => <li key={id}><Link to={`/class/${id}`}>{name}</Link></li>)}
+                        </ul>
+                    </nav>
+                )}
+            </Query>
             <hr />
             <AddClass />
         </ScreenLayout>
