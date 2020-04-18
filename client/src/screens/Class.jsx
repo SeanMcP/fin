3a8fb56b06a40b8179dcfc4e9fb2e5e2635a8160
Sean McPherson <git@seanmcp.com>
2020-04-18 14:41:58 -0400

    Add remove from class button

--- client/src/screens/Class.jsx
+++ client/src/screens/Class.jsx
@@ -3,6 +3,17 @@ import { useParams } from 'react-router-dom'
 import AddStudent from '../components/AddStudent'
 import Query from '../components/Query'
 import ScreenLayout from '../components/ScreenLayout'
+import request from '../utils/request'
+
+async function removeFromClass(studentId, classId) {
+    const response = await request(`seats/student/${studentId}/class/${classId}`, { method: `DELETE` })
+
+    if (response.ok) {
+        alert('Student removed from class!')
+    } else {
+        alert('Uh oh! Something went wrong.')
+    }
+}
 
 function Class() {
     const { id } = useParams()
@@ -17,7 +28,12 @@ function Class() {
                 }}
             </Query>
             <AddStudent classId={id} />
-            <Query id="students-in-this-class" route={`/seats/class/${id}`} />
+            <h2>Students</h2>
+            <Query id="students-in-this-class" route={`/seats/class/${id}`}>
+                {({ data }) => {
+                    return <ul>{data.map(({ name, student_id: studentId }) => <li key={studentId}>{name}<button onClick={() => removeFromClass(studentId, id)}>Remove</button></li>)}</ul>
+                }}
+            </Query>
         </ScreenLayout>
     )
 }
