a73fc2b496abaaf21b84ae58b02a01060d133d60
Sean McPherson <git@seanmcp.com>
2020-04-21 06:57:10 -0400

    Open /register endpoint
    I don't know how things like this slip through the cracks.

--- api/middlewares/jwt.js
+++ api/middlewares/jwt.js
@@ -5,4 +5,4 @@ module.exports = jwt({
     // TODO: Figure out where to store secret
     secret: 'keep_it_secret_keep_it_safe'
 })
-    .unless({ path: ['/', '/clear', '/health', '/login', '/refresh'] })
+    .unless({ path: ['/', '/clear', '/health', '/login', '/refresh', '/register'] })
