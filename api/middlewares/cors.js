5a2088e7af2dfbf68344cb0171836fa31de44f41
Sean McPherson <git@seanmcp.com>
2020-04-25 15:40:05 -0400

    Add extension environment variables

--- api/middlewares/cors.js
+++ api/middlewares/cors.js
@@ -1,6 +1,10 @@
 const cors = require('cors')
 
 module.exports = cors({
-    credentials: true,
-    origin: process.env.CLIENT_URL,
+  credentials: true,
+  origin: [
+    process.env.CLIENT_URL,
+    process.env.EXTENSION_URL,
+    process.env.NODE_ENV === 'development' ? /chrome-extension:/ : undefined,
+  ],
 })
