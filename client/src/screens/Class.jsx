5fefaddd22cdcb6603e99b6a9046d79cd01d55ff
Sean McPherson <git@seanmcp.com>
2020-04-25 07:22:08 -0400

    Check for data  before rendering
    I'm disliking the Query component more and more as time goes on.

--- client/src/screens/Class.jsx
+++ client/src/screens/Class.jsx
@@ -6,36 +6,49 @@ import ScreenLayout from '../components/ScreenLayout'
 import request from '../utils/request'
 
 async function removeFromClass(studentId, classId) {
-    const response = await request(`seats/student/${studentId}/class/${classId}`, { method: `DELETE` })
+  const response = await request(
+    `seats/student/${studentId}/class/${classId}`,
+    { method: `DELETE` },
+  )
 
-    if (response.ok) {
-        alert('Student removed from class!')
-    } else {
-        alert('Uh oh! Something went wrong.')
-    }
+  if (response.ok) {
+    alert('Student removed from class!')
+  } else {
+    alert('Uh oh! Something went wrong.')
+  }
 }
 
 function Class() {
-    const { id } = useParams()
-    return (
-        <ScreenLayout title="Class">
-            <Query id="class" route={`/class/${id}`}>
-                {({ data }) => {
-                    const { name } = data[0]
-                    return (
-                        <h1>{name}</h1>
-                    )
-                }}
-            </Query>
-            <AddStudent classId={id} />
-            <h2>Students</h2>
-            <Query id="students-in-this-class" route={`/seats/class/${id}`}>
-                {({ students }) => {
-                    return <ul>{students.map(({ name, id: studentId }) => <li key={studentId}>{name}<button onClick={() => removeFromClass(studentId, id)}>Remove</button></li>)}</ul>
-                }}
-            </Query>
-        </ScreenLayout>
-    )
+  const { id } = useParams()
+  return (
+    <ScreenLayout title="Class">
+      <Query id="class" route={`/class/${id}`}>
+        {({ data }) => {
+          if (!data) return null
+          const { name } = data[0]
+          return <h1>{name}</h1>
+        }}
+      </Query>
+      <AddStudent classId={id} />
+      <h2>Students</h2>
+      <Query id="students-in-this-class" route={`/seats/class/${id}`}>
+        {({ students }) => {
+          return (
+            <ul>
+              {students.map(({ name, id: studentId }) => (
+                <li key={studentId}>
+                  {name}
+                  <button onClick={() => removeFromClass(studentId, id)}>
+                    Remove
+                  </button>
+                </li>
+              ))}
+            </ul>
+          )
+        }}
+      </Query>
+    </ScreenLayout>
+  )
 }
 
 export default Class
