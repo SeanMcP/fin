e3fb565a5059cc70cd7815ce79bc47d4e91d78d3
Sean McPherson <git@seanmcp.com>
2020-04-29 07:00:46 -0400

    chore: run formatter

--- client/src/components/LogIn.js
+++ client/src/components/LogIn.js
@@ -5,39 +5,57 @@ import { useAppContext, APP_ACTIONS } from '../store/AppContext'
 import request from '../utils/request'
 
 function LogIn() {
-    const [, dispatch] = useAppContext()
-    const history = useHistory()
+  const [, dispatch] = useAppContext()
+  const history = useHistory()
 
-    const formik = useFormik({
-        initialValues: {
-            email: 'chuck@testa.com',
-            password: 'nope'
-        },
-        async onSubmit({ email, password }) {
-            const response = await request('login', {
-                body: { email, password }
-            })
+  const formik = useFormik({
+    initialValues: {
+      email: 'chuck@testa.com',
+      password: 'nope',
+    },
+    async onSubmit({ email, password }) {
+      const response = await request('login', {
+        body: { email, password },
+      })
 
-            if (response.ok) {
-                const { user } = await response.json()
-                dispatch({ type: APP_ACTIONS.LOG_IN, payload: { user } })
-                history.push('/dashboard')
-            }
-        }
-    })
+      if (response.ok) {
+        const { user } = await response.json()
+        dispatch({ type: APP_ACTIONS.LOG_IN, payload: { user } })
+        history.push('/dashboard')
+      }
+    },
+  })
 
-    return (
-        <form onSubmit={formik.handleSubmit}>
-            <h2>Log In</h2>
-            <div>
-                <label>Email<br /><input type="text" name="email" value={formik.values.email} onChange={formik.handleChange} /></label>
-            </div>
-            <div>
-                <label>Password<br /><input type="password" name="password" value={formik.values.password} onChange={formik.handleChange} /></label>
-            </div>
-            <button>Log In</button>
-        </form>
-    )
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <h2>Log In</h2>
+      <div>
+        <label>
+          Email
+          <br />
+          <input
+            type="text"
+            name="email"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Password
+          <br />
+          <input
+            type="password"
+            name="password"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+          />
+        </label>
+      </div>
+      <button>Log In</button>
+    </form>
+  )
 }
 
 export default LogIn
