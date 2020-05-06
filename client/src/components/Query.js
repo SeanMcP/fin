9561b4c868bc50c717d2bb395186c02193130354
Sean McPherson <git@seanmcp.com>
2020-05-06 06:43:54 -0400

    feat: handle errors

--- client/src/components/Query.js
+++ client/src/components/Query.js
@@ -2,21 +2,46 @@ import React from 'react'
 import { useQuery } from 'react-query'
 
 function Query({ route, ...props }) {
-    const { status, data, error } = useQuery(props.id, () =>
-        fetch(`${process.env.REACT_APP_API_URL}/${route[0] === '/' ? route.slice(1) : route}`, { credentials: 'include' }).then(response => response.json())
+  const { status, data, error } = useQuery(props.id, () =>
+    fetch(
+      `${process.env.REACT_APP_API_URL}/${
+        route[0] === '/' ? route.slice(1) : route
+      }`,
+      { credentials: 'include' },
+    )
+      .then((response) => {
+        if (response.ok) return response.json()
+        throw Error('Uh oh!')
+      })
+      .catch(Promise.reject),
+  )
+
+  if (status === 'loading') {
+    return typeof props.onLoading === 'function' ? (
+      props.onLoading()
+    ) : (
+      <span>Loading...</span>
+    )
+  }
+
+  if (status === 'error') {
+    return typeof props.onError === 'function' ? (
+      props.onError(error)
+    ) : (
+      <span>Uh oh! There was a problem loading.</span>
+    )
+  }
+
+  if (data)
+    return typeof props.children === 'function' ? (
+      props.children(data)
+    ) : (
+      <span>
+        Data: <pre>{JSON.stringify(data, null, 2)}</pre>
+      </span>
     )
 
-    if (status === 'loading') {
-        return typeof props.onLoading === 'function' ? props.onLoading() : <span>Loading...</span>
-    }
-
-    if (status === 'error') {
-        return typeof props.onError === 'function' ? props.onError(error) : <span>Uh oh! There was a problem loading.</span>
-    }
-
-    if (data) return typeof props.children === 'function' ? props.children(data) : <span>Data: <pre>{JSON.stringify(data, null, 2)}</pre></span>
-
-    return <span>Something went wrong</span>
+  return <span>Something went wrong</span>
 }
 
 export default Query
