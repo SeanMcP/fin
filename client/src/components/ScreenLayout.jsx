e249cc323f72e4b5df817ece723982f911584b91
Sean McPherson <git@seanmcp.com>
2020-05-07 21:49:02 -0400

    style: basic positioning

--- client/src/components/ScreenLayout.jsx
+++ client/src/components/ScreenLayout.jsx
@@ -4,30 +4,38 @@ import { useAppContext } from '../store/AppContext'
 import LogOut from './LogOut'
 
 function ScreenLayout(props) {
-    const [{ isAuthenticated }] = useAppContext()
-    useEffect(() => {
-        let title = 'Fin'
-        if (props.title) {
-            title = props.title + ' - ' + title
-        }
-        document.title = title
-    }, [props.title])
+  const [{ isAuthenticated }] = useAppContext()
+  useEffect(() => {
+    let title = 'Fin'
+    if (props.title) {
+      title = props.title + ' - ' + title
+    }
+    document.title = title
+  }, [props.title])
 
-    return (
-        <div>
-            <header>
-                <Link to="/">Fin</Link>
-                {isAuthenticated && (
-                    <nav aria-label="main navigation">
-                        <Link to="/dashboard">Dashboard</Link>
-                        <Link to="/students">Students</Link>
-                        <LogOut />
-                    </nav>
-                )}
-            </header>
-            <main>{props.children}</main>
+  return (
+    <div className="ScreenLayout">
+      <header>
+        <div className="content">
+          <Link to="/">Fin</Link>
+          {isAuthenticated && (
+            <React.Fragment>
+              <nav aria-label="main navigation">
+                <Link to="/dashboard">Dashboard</Link>
+                <Link to="/students">Students</Link>
+              </nav>
+              <div className="account">
+                <LogOut />
+              </div>
+            </React.Fragment>
+          )}
         </div>
-    )
+      </header>
+      <main id="main">
+        <div className="content">{props.children}</div>
+      </main>
+    </div>
+  )
 }
 
 export default ScreenLayout
