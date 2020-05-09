4d70ce7cee7953d4cf6ff8c83c212e5303ed8091
Sean McPherson <git@seanmcp.com>
2020-05-09 14:59:44 -0400

    feat: remove index, list stores
    These variables only need to be in the picker view.

--- extension/src/stores.js
+++ extension/src/stores.js
@@ -1,4 +1,3 @@
-import shuffle from 'array-shuffle'
 import { writable } from 'svelte/store'
 import { ROUTES } from './routes'
 
@@ -15,50 +14,4 @@ export const location = createLocation()
 
 export const userId = writable('')
 
-export const currentSectionId = writable('')
-
-function createCurrentList() {
-  const store = writable([])
-
-  return {
-    length: store.subscribe.length,
-    set(array) {
-      store.set(shuffle(array))
-    },
-    shuffle() {
-      store.update((arr) => shuffle(arr))
-    },
-    ...store,
-  }
-}
-
-export const currentList = createCurrentList()
-
-function createIndex() {
-  const store = writable(0)
-  return {
-    decrement: () => {
-      store.update((index) => {
-        let next = index - 1
-        if (next <= 0) {
-          next = currentList.length - 1
-          currentList.shuffle()
-        }
-        return next
-      })
-    },
-    increment: () => {
-      store.update((index) => {
-        let next = index + 1
-        if (next >= currentList.length) {
-          next = 0
-          currentList.shuffle()
-        }
-        return next
-      })
-    },
-    ...store,
-  }
-}
-
-export const currentIndex = createIndex()
+export const sectionId = writable('')
