89bd33b60f7bfe91577dfa0f73d10e6d45e93295
Sean McPherson <git@seanmcp.com>
2020-05-11 06:57:55 -0400

    feat: allow extension requests from web

--- api/middlewares/extension-only.js
+++ api/middlewares/extension-only.js
@@ -4,10 +4,14 @@ const originBrowserMap = {
 
 module.exports = function (req, res, next) {
   const { headers } = req
+
+  if (headers.origin === process.env.EXTENSION_URL) return next()
+
   const [origin] = headers.origin.split('://')
   if (originBrowserMap[origin]) {
     res.extension = originBrowserMap[origin]
     return next()
   }
+
   res.status(401)
 }
