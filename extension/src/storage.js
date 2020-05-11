61fe71bc61196f92399b4d869cc0d6897c39bb52
Sean McPherson <git@seanmcp.com>
2020-05-11 07:05:23 -0400

    feat: add web storage

--- extension/src/storage.js
+++ extension/src/storage.js
@@ -1,10 +1,20 @@
 const isChrome = typeof chrome !== 'undefined'
+const isWeb = typeof localStorage !== 'undefined'
 
 /** Async: Takes an array of keys and returns a promise with the result */
 export function get(keyArray) {
   if (isChrome) {
     return new Promise((resolve) => {
       chrome.storage.local.get(keyArray, resolve)
+    })
+  } else if (isWeb) {
+    return new Promise((resolve) => {
+      const response = {}
+      keyArray.forEach(key => {
+        const value = JSON.parse(localStorage.getItem(key))
+        if (value != null) response[key] = value
+      })
+      resolve(response)
     })
   }
 }
@@ -18,6 +28,11 @@ export function remove(keyArray) {
     return new Promise((resolve) => {
       chrome.storage.local.remove(keyArray, resolve)
     })
+  } else if (isWeb) {
+    return new Promise((resolve) => {
+      keyArray.forEach(key => localStorage.removeItem(key))
+      resolve()
+    })
   }
 }
 
@@ -26,6 +41,13 @@ export function set(updateObject) {
   if (isChrome) {
     return new Promise((resolve) => {
       chrome.storage.local.set(updateObject, resolve)
+    })
+  } else if (isWeb) {
+    return new Promise((resolve) => {
+      for (const [key, value] of Object.entries(updateObject)) {
+        localStorage.setItem(key, JSON.stringify(value))
+      }
+      resolve()
     })
   }
 }
