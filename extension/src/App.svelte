a32cbb9e20892e2c828893e56e6ba8d88629932a
Sean McPherson <git@seanmcp.com>
2020-05-12 06:50:09 -0400

    feat: add route, remove store

--- extension/src/App.svelte
+++ extension/src/App.svelte
@@ -1,28 +1,29 @@
 <script>
 	import { onMount } from 'svelte'
-	import Sections from './Sections.svelte'
 	import LogIn from './LogIn.svelte'
 	import Picker from './Picker.svelte'
+	import Section from './Section.svelte'
+	import Sections from './Sections.svelte'
 	import { ROUTES } from './routes'
-	import { location, sectionId, userId } from './stores'
+	import { location, userId } from './stores'
 	import { get } from './storage'
 
 	const router = {
-		[ROUTES.sections]: Sections,
 		[ROUTES.login]: LogIn,
+		[ROUTES.section]: Section,
+		[ROUTES.sections]: Sections,
 		[ROUTES.picker]: Picker,
 	}
 
 	onMount(async () => {
-		const result = await get(['index', 'list', 'sectionId', 'userId'])
+		const result = await get(['index', 'list', 'section', 'userId'])
 
 		// Hydrate store with values from storage
-		if (result.sectionId) sectionId.set(result.sectionId)
 		if (result.userId) userId.set(result.userId)
 
 		// If there are stored values for index and list, then the user
 		// was previously in the picker mode. Send them back.
-		if (result.index != null && result.list && result.sectionId) {
+		if (result.index != null && result.list && result.section) {
 			location.navigate(ROUTES.picker)
 		// If they are logged in, send them to the sections page.
 		} else if (result.userId) {
